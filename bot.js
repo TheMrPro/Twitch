57fb1c5b1173974f6211d60495cad283b55b81ca
TheMrPro <40274555+TheMrPro@users.noreply.github.com>
2018-11-23 22:48:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#Vade'Community`,"http://twitch.tv/Dream")
+client.user.setGame(`#Vade'Community`,"https://www.twitch.tv/immohamed_x")
 client.user.setStatus("dnd")
 });
 
